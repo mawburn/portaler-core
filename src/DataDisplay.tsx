15291c30fdb3c932c80428e8efd632c0a3f70d03
mawburn <mawburn7@gmail.com>
2020-09-27 07:16:16 -0400

    fix layout issues

--- src/DataDisplay.tsx
+++ src/DataDisplay.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react'
-import { Zone, Portal } from './types'
-import CytoscapeComponent from 'react-cytoscapejs'
 import { ElementDefinition } from 'cytoscape'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import CytoscapeComponent from 'react-cytoscapejs'
+
+import { Portal, Zone } from './types'
 
 interface DataDisplayProps {
   zones: Zone[]
@@ -23,15 +24,13 @@ const zoneColorToColor = {
   road: 'lightblue',
 }
 
-const defaultLayout = 'breadthfirst'
-
 const DataDisplay: React.FC<DataDisplayProps> = ({
   zones,
   portals,
   onNodeClick,
 }) => {
   const mapContainer = useRef<HTMLDivElement>(null)
-  const [layout, setLayout] = useState('random')
+  const [layout, setLayout] = useState('breadthfirst')
 
   const filteredZones = zones.filter(
     (z) => !!portals.find((p) => p.source === z.name || p.target === z.name)
@@ -49,37 +48,33 @@ const DataDisplay: React.FC<DataDisplayProps> = ({
     [onNodeClick, setActiveZoneName]
   )
 
-  useEffect(() => {
-    setTimeout(() => setLayout(defaultLayout), 500)
-  }, [])
+  const data: ElementDefinition[] = [
+    ...filteredZones.map((z) => ({
+      data: { id: z.name, label: z.name },
+      style: {
+        backgroundColor: zoneColorToColor[z.color],
+        shape: z.type.indexOf('TUNNEL_HIDEOUT') >= 0 ? 'pentagon' : '',
+      },
+    })),
+    ...portals.map((p) => ({
+      data: {
+        source: p.source,
+        target: p.target,
+        label: `${Math.floor(p.timeLeft / 60)}h ${Math.round(
+          p.timeLeft % 60
+        )}min`,
+      },
+      classes: p.timeLeft < 30 ? 'timeLow' : '',
+      style: {
+        lineColor: portalSizeToColor[p.size],
+      },
+    })),
+  ]
 
-  if (zones.length > 0) {
-    const data: ElementDefinition[] = [
-      ...filteredZones.map((z) => ({
-        data: { id: z.name, label: z.name },
-        style: {
-          backgroundColor: zoneColorToColor[z.color],
-          shape: z.type.indexOf('TUNNEL_HIDEOUT') >= 0 ? 'pentagon' : '',
-        },
-      })),
-      ...portals.map((p) => ({
-        data: {
-          source: p.source,
-          target: p.target,
-          label: `${Math.floor(p.timeLeft / 60)}h ${Math.round(
-            p.timeLeft % 60
-          )}min`,
-        },
-        classes: p.timeLeft < 30 ? 'timeLow' : '',
-        style: {
-          lineColor: portalSizeToColor[p.size],
-        },
-      })),
-    ]
-
-    return (
-      <>
-        <div className="h100" ref={mapContainer}>
+  return (
+    <>
+      <div className="h100" ref={mapContainer}>
+        {zones.length && portals.length ? (
           <CytoscapeComponent
             elements={data}
             cy={(cy) => {
@@ -111,51 +106,52 @@ const DataDisplay: React.FC<DataDisplayProps> = ({
             ]}
             layout={{ name: layout }}
           />
-        </div>
-        <select onChange={(e) => setLayout(e.target.value)} value={layout}>
-          <option value="random">random</option>
-          <option value="grid">grid</option>
-          <option value="circle">circle</option>
-          <option value="cose">cose</option>
-          <option value="concentric">concentric</option>
-          <option value="breadthfirst">breadthfirst</option>
-        </select>
-        {activeZone && (
-          <table>
-            <tbody>
-              <tr>
-                <td>Name</td>
-                <td>{activeZone.name}</td>
-              </tr>
-              <tr>
-                <td>Type</td>
-                <td>{activeZone.type}</td>
-              </tr>
-              <tr>
-                <td>Resources</td>
-                <td>
-                  {activeZone.resources &&
-                    activeZone.resources
-                      .map((r) => `T${r.tier} ${r.name}`)
-                      .join(', ')}
-                </td>
-              </tr>
-              <tr>
-                <td>Map markers</td>
-                <td>{activeZone.markers && activeZone.markers.join(', ')}</td>
-              </tr>
-            </tbody>
-          </table>
+        ) : (
+          <div className="cyto">Loading</div>
         )}
-      </>
-    )
-  }
-  return (
-    <div>
-      Unable to fetch zone information from the server, this could be a network
-      issue, or invalid password
-    </div>
+      </div>
+      <select onChange={(e) => setLayout(e.target.value)} value={layout}>
+        <option value="random">random</option>
+        <option value="grid">grid</option>
+        <option value="circle">circle</option>
+        <option value="cose">cose</option>
+        <option value="concentric">concentric</option>
+        <option value="breadthfirst">breadthfirst</option>
+      </select>
+      {activeZone && (
+        <table>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{activeZone.name}</td>
+            </tr>
+            <tr>
+              <td>Type</td>
+              <td>{activeZone.type}</td>
+            </tr>
+            <tr>
+              <td>Resources</td>
+              <td>
+                {activeZone.resources &&
+                  activeZone.resources
+                    .map((r) => `T${r.tier} ${r.name}`)
+                    .join(', ')}
+              </td>
+            </tr>
+            <tr>
+              <td>Map markers</td>
+              <td>{activeZone.markers && activeZone.markers.join(', ')}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
+    </>
   )
 }
+
+// ;<div>
+//   Unable to fetch zone information from the server, this could be a network
+//   issue, or invalid password
+// </div>
 
 export default DataDisplay
