948ee2d7b354ae2a2a2704dd0a6ab51259964c8d
matt burnett <mawburn@users.noreply.github.com>
2020-10-01 08:14:55 -0400

    Update src/App/useGetZones.ts
    Co-authored-by: Richard Weber <richard.weber@nitor.com>

--- src/App/useGetZones.ts
+++ src/App/useGetZones.ts
@@ -7,15 +7,13 @@ const useGetZones = (token: string, isPublic?: boolean) => {
 
   useEffect(() => {
     if (token !== '' || isPublic) {
-      ;(async () => {
-        const res = await fetch(`/api/zone`, {
+      fetch(`/api/zone`, {
           headers: {
             'X-Tebro-Auth': token,
           },
-        }).then((r) => r.json())
-
-        setZones(res)
-      })()
+        })
+        .then((r) => r.json())
+        .then(setZones);
     }
   }, [token, isPublic])
 
