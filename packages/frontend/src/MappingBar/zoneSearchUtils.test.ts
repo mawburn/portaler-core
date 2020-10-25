f37e5c89790285d9724f5a0ef037f225543bb9ba
mawburn <mawburn7@gmail.com>
2020-10-24 20:42:03 -0400

    linting & sass mostly

--- packages/frontend/src/MappingBar/zoneSearchUtils.test.ts
+++ packages/frontend/src/MappingBar/zoneSearchUtils.test.ts
@@ -1,12 +1,12 @@
-import { FilterOptionsState } from '@material-ui/lab/useAutocomplete';
-import { filterZones, getMaxString, ZoneLight } from './zoneSearchUtils';
+import { FilterOptionsState } from '@material-ui/lab/useAutocomplete'
+import { filterZones, getMaxString, ZoneLight } from './zoneSearchUtils'
 
 describe('Test zoneSearchUtils', () => {
   describe('Test filterZones', () => {
     const getState = (inputValue: string): FilterOptionsState<ZoneLight> => ({
       inputValue,
       getOptionLabel: () => '',
-    });
+    })
 
     const testList: ZoneLight[] = [
       { name: 'Stinkhag', value: 'stinkhag' },
@@ -30,35 +30,35 @@ describe('Test zoneSearchUtils', () => {
       { name: 'PSG-0041', value: 'psg-0041' },
       { name: 'Secent-Qi-Odesom', value: 'secent-qi-odesom' },
       { name: 'Sectun-In-Qinsom', value: 'sectun-in-qinsom' },
-    ];
+    ]
 
     test('starts with', () => {
-      expect(filterZones(testList, getState('sectun')).length).toBe(2);
-      expect(filterZones(testList, getState('SeCtuN')).length).toBe(2);
-      expect(filterZones(testList, getState('SeCtuN-I')).length).toBe(1);
-      expect(filterZones(testList, getState('Bank o')).length).toBe(2);
-      expect(filterZones(testList, getState('xxx')).length).toBe(0);
-    });
+      expect(filterZones(testList, getState('sectun')).length).toBe(2)
+      expect(filterZones(testList, getState('SeCtuN')).length).toBe(2)
+      expect(filterZones(testList, getState('SeCtuN-I')).length).toBe(1)
+      expect(filterZones(testList, getState('Bank o')).length).toBe(2)
+      expect(filterZones(testList, getState('xxx')).length).toBe(0)
+    })
 
     test('fuzzy search', () => {
-      expect(filterZones(testList, getState('sec qi')).length).toBe(3);
-      expect(filterZones(testList, getState('p 0')).length).toBe(2);
-      expect(filterZones(testList, getState('BA O')).length).toBe(2);
-      expect(filterZones(testList, getState('xxx')).length).toBe(0);
-    });
+      expect(filterZones(testList, getState('sec qi')).length).toBe(3)
+      expect(filterZones(testList, getState('p 0')).length).toBe(2)
+      expect(filterZones(testList, getState('BA O')).length).toBe(2)
+      expect(filterZones(testList, getState('xxx')).length).toBe(0)
+    })
 
     test('Test single entry', () => {
       const singleList = [
         { name: 'HomeTerritory Skirmish', value: 'hometerritory skirmish' },
-      ];
+      ]
 
-      expect(filterZones(singleList, getState('ho'))).toMatchObject(singleList);
-    });
+      expect(filterZones(singleList, getState('ho'))).toMatchObject(singleList)
+    })
 
     test('Test empty list', () => {
-      expect(filterZones([], getState('xx')).length).toBe(0);
-    });
-  });
+      expect(filterZones([], getState('xx')).length).toBe(0)
+    })
+  })
 
   describe('Test getMaxString', () => {
     test('Test common cases', () => {
@@ -66,42 +66,42 @@ describe('Test zoneSearchUtils', () => {
         { name: 'Sectun-Qinsom', value: 'sectun-qinsom' },
         { name: 'Secent-Qi-Odesom', value: 'secent-qi-odesom' },
         { name: 'Sectun-In-Qinsom', value: 'sectun-in-qinsom' },
-      ];
+      ]
 
-      expect(getMaxString(testList1, 'se')).toBe('Sec');
-      expect(getMaxString(testList1, 'sEc')).toBe('Sec');
+      expect(getMaxString(testList1, 'se')).toBe('Sec')
+      expect(getMaxString(testList1, 'sEc')).toBe('Sec')
 
       const testList2: ZoneLight[] = [
         { name: 'Bank of Thetford', value: 'bank of thetford' },
         { name: 'Bank of Lymhurst', value: 'bank of lymhurst' },
         { name: 'Bank of Batman', value: 'bank of batman' },
         { name: 'Bank of Joker', value: 'bank of joker' },
-      ];
+      ]
 
-      expect(getMaxString(testList2, 'b')).toBe('Bank of ');
-      expect(getMaxString(testList2, 'Ban')).toBe('Bank of ');
-      expect(getMaxString(testList2, 'bAnk o')).toBe('Bank of ');
+      expect(getMaxString(testList2, 'b')).toBe('Bank of ')
+      expect(getMaxString(testList2, 'Ban')).toBe('Bank of ')
+      expect(getMaxString(testList2, 'bAnk o')).toBe('Bank of ')
 
       const testList3: ZoneLight[] = [
         { name: 'Sectun-Qinsom', value: 'sectun-qinsom' },
         { name: 'Sectun-Qi-Odesom', value: 'secent-qi-odesom' },
         { name: 'Sectun-Qi-Qinsom', value: 'sectun-in-qinsom' },
-      ];
+      ]
 
-      expect(getMaxString(testList3, 'se')).toBe('Sectun-Qi');
-      expect(getMaxString(testList3.slice(1, 3), 'Se')).toBe('Sectun-Qi-');
-    });
+      expect(getMaxString(testList3, 'se')).toBe('Sectun-Qi')
+      expect(getMaxString(testList3.slice(1, 3), 'Se')).toBe('Sectun-Qi-')
+    })
 
     test('Test single entry', () => {
       const singleList = [
         { name: 'HomeTerritory Skirmish', value: 'hometerritory skirmish' },
-      ];
+      ]
 
-      expect(getMaxString(singleList, 'ho')).toBe('HomeTerritory Skirmish');
-    });
+      expect(getMaxString(singleList, 'ho')).toBe('HomeTerritory Skirmish')
+    })
 
     test('Test empty list', () => {
-      expect(getMaxString([], 'xx')).toBe('xx');
-    });
-  });
-});
+      expect(getMaxString([], 'xx')).toBe('xx')
+    })
+  })
+})
--- packages/main-api/src/server.ts
+++ packages/main-api/src/server.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express'
 
-const app = express();
+const app = express()
 
 app.listen(4000, () => {
-  console.info('Running on port 4000');
-});
+  console.info('Running on port 4000')
+})
 
 // Routes
-app.use('/api/discord', require('./module/discord'));
+app.use('/api/discord', require('./module/discord'))
 
 // middlewares should probably be moved eventually
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -16,29 +16,29 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
       return res.status(400).send({
         status: 'ERROR',
         error: err.message,
-      });
+      })
     default:
       return res.status(500).send({
         status: 'ERROR',
         error: err.message,
-      });
+      })
   }
-});
+})
 
 // validate that they have a token
 app.use((req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization?.split(' ')[1]
     // TODO do a token lookup here, to get server and auth info on this user
 
     if (token) {
-      return next();
+      return next()
     }
   } catch {
     // if the user has no token, redirect to login
-    res.redirect('/');
-    return;
+    res.redirect('/')
+    return
   }
   // if the try does not complete, then lets redirect to login
-  res.redirect('/');
-});
+  res.redirect('/')
+})
