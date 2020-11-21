99087b94de2f6fe918f9a069680f0a0fa51cea81
mawburn <mawburn7@gmail.com>
2020-11-20 23:12:41 -0500

    add connections table

--- packages/discord-bot/db_migrations/003_populate-zones.sql
+++ packages/discord-bot/db_migrations/003_populate-zones.sql
@@ -10,6 +10,13 @@ CREATE TABLE IF NOT EXISTS zones (
 
 CREATE INDEX idx_zone_name_color ON zones (zone_name, color);
 
+CREATE TABLE IF NOT EXISTS royal_connections (
+	zone_one INT NOT NULL,
+	zone_two INT NOT NULL,
+	conn_type VARCHAR(10) NOT NULL DEFAULT 'normal',
+	PRIMARY KEY (zone_one, zone_two)
+);
+
 INSERT INTO zones (albion_id, zone_name, tier, zone_type, color) VALUES
 	('4300', 'Arthur''s Rest', 'I', 'PLAYERCITY_BLACK_NOFURNITURE', 'city'),
 	('2009', 'Astolat', 'VI', 'OPENPVP_RED', 'red'),
