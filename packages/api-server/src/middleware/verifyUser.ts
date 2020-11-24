60432042a94cc8fe1e1391171044bb23740990fd
Richard Weber <riche.weber@gmail.com>
2020-11-25 00:42:37 +0200

    Refactor async handlers (#22)
    * Refactor async handlers
    
    * fix local dev issues
    
    Co-authored-by: mawburn <mawburn7@gmail.com>

--- packages/api-server/src/middleware/verifyUser.ts
+++ packages/api-server/src/middleware/verifyUser.ts
@@ -2,61 +2,53 @@ import { NextFunction, Request, Response } from 'express'
 
 import { redis } from '../db'
 import logger from '../logger'
-import wrapAsync from './wrapAsync'
 
-export interface AuthRequest extends Request {
-  userId: number
-  serverId: number
-}
-
-const verifyUser = wrapAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      if (process.env.DISABLE_AUTH === 'true') {
-        ;(req as AuthRequest).userId = 1
-        ;(req as AuthRequest).serverId = 1
-        next()
-        return
-      }
-
-      if (!req.headers.authorization) {
-        return res.sendStatus(401)
-      }
-
-      const authHeaders = req.headers.authorization.split(' ')
-
-      if (authHeaders[0] !== 'Bearer') {
-        return res.sendStatus(401)
-      }
-
-      const token = authHeaders[1]
-
-      const userServer = await redis.getUser(token)
-
-      if (!userServer) {
-        return res.sendStatus(403)
-      }
-
-      const [userId, serverId] = userServer.split(':')
-
-      const subdomain = await redis.getAsync(`server:${serverId}`)
-
-      if (
-        process.env.NODE_ENV === 'production' &&
-        subdomain !== req.subdomains[0]
-      ) {
-        return res.sendStatus(403)
-      }
-
-      ;(req as AuthRequest).userId = Number(userId)
-      ;(req as AuthRequest).serverId = Number(serverId)
-
+const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (process.env.DISABLE_AUTH === 'true') {
+      req.userId = 1
+      req.serverId = 1
       next()
-    } catch (err) {
-      logger.log.warn('Error verifying user', err)
-      return res.status(500).send({ error: 'Error Verifying User' })
+      return
     }
+
+    if (!req.headers.authorization) {
+      return res.sendStatus(401)
+    }
+
+    const authHeaders = req.headers.authorization.split(' ')
+
+    if (authHeaders[0] !== 'Bearer') {
+      return res.sendStatus(401)
+    }
+
+    const token = authHeaders[1]
+
+    const userServer = await redis.getUser(token)
+
+    if (!userServer) {
+      return res.sendStatus(403)
+    }
+
+    const [userId, serverId] = userServer.split(':')
+
+    const subdomain = await redis.getAsync(`server:${serverId}`)
+
+    if (
+      process.env.NODE_ENV === 'production' &&
+      subdomain !== req.subdomains[0]
+    ) {
+      return res.sendStatus(403)
+    }
+
+    req.userId = Number(userId)
+    req.serverId = Number(serverId)
+
+    next()
+  } catch (err) {
+    logger.log.warn('Error verifying user', err)
+    return res.status(500).send({ error: 'Error Verifying User' })
   }
-)
+}
 
 export default verifyUser
