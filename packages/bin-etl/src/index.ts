cf0b62cf2a5d2d422d4ada05097c575e28d919f7
mmcken3 <mitchell.mckenzie95@gmail.com>
2020-11-28 09:58:31 -0500

    Fix a few of the style changes

--- packages/bin-etl/src/index.ts
+++ packages/bin-etl/src/index.ts
@@ -13,8 +13,8 @@ function processFile(n: string) {
 }
 
 function handleJson(data: any) {
-    const cluster = data["world"]["clusters"]["cluster"]
-    const selectedFields = new Array()
+    const cluster = data.world.clusters.cluster
+    const selectedFields = []
     for (let m of cluster) {
       const sf = {
         "name": m["@displayname"],
@@ -29,20 +29,14 @@ function handleJson(data: any) {
 }
 
 function isValidZone(sf: any) {
-  const type: String = sf["type"]
+  const type: String = sf.type
   return (type.startsWith("TUNNEL") || type.startsWith("OPENPVP") || type.startsWith("SAFEAREA"))
 }
 
 function extractResources(m: any) {
-  if (("distribution" in m) && ("resource" in m["distribution"])) {
-    return m["distribution"]["resource"]
-  }
-  return []
+  return m.distribution?.resource ?? []
 }
 
 function extractMapMarkers(m: any) {
-  if ("minimapmarkers" in m) {
-    return m["minimapmarkers"]["marker"]
-  }
-  return []
+  return m.minimapmarkers?.marker ?? []
 }
