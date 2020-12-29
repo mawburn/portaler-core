49abcdf94306a0d53ab96ff2a44c5cdc0f1fbd08
mawburn <mawburn@users.noreply.github.com>
2020-12-29 06:14:36 +0000

    Update homepage to output generated at 4e7fb6a

---
+++ component---src-pages-404-tsx-0db9048f5ad5d37c281e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("ERkP"),u=n.n(a),o=n("VXBa"),l=n("H8eV");t.default=function(){return u.a.createElement(o.a,null,u.a.createElement(l.a,{title:"404: Not found"}),u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-0db9048f5ad5d37c281e.js.map
