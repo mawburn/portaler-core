381d6f5d0f0d4956e8f7203a7a32c1665b646c8b
mawburn <mawburn7@gmail.com>
2021-02-02 07:37:36 -0500

    update footer

--- packages/frontend/src/App/Footer/index.tsx
+++ packages/frontend/src/App/Footer/index.tsx
@@ -1,13 +1,36 @@
 import React from 'react'
-import { discordLogo, githubLogo } from '../../common/images'
+import { discordLogo, githubLogo, twitterLogo } from '../../common/images'
 import Twitch from './Twitch'
 
 import styles from './styles.module.scss'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <Twitch />
-    <div className={styles.footerRight}>
+    <div className={styles.left}>
+      <Twitch />
+    </div>
+    <div className={styles.middle}>
+      <a
+        href="https://www.patreon.com/portaler?fan_landing=true"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://camo.githubusercontent.com/4d19a0333fc372e9b8ca441d067af4fe011e50d1ce7da44975106f493397384c/68747470733a2f2f692e696d6775722e636f6d2f6c79336c616c7a2e706e67"
+          className={styles.patreonLogo}
+        />
+      </a>
+    </div>
+    <div className={styles.right}>
+      <div className={styles.twitter}>
+        <a
+          href="https://twitter.com/PortalerZone"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={twitterLogo} className={styles.twitterLogo} alt="twitter" />
+        </a>
+      </div>
       <div className={styles.github}>
         <a
           href="https://github.com/Portaler-Zone/portaler-core"
@@ -15,7 +38,6 @@ const Footer = () => (
           rel="noopener noreferrer"
         >
           <img src={githubLogo} className={styles.githubLogo} alt="github" />
-          GitHub
         </a>
       </div>
       <div className={styles.discord}>
