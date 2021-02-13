c1ad108138ccf2d499d79b3cf048bafa3d6461f0
mawburn <mawburn@users.noreply.github.com>
2021-02-13 07:36:02 +0000

    Update file(s) "/." from "Portaler-Zone/portaler-core"

--- packages/discord-bot/src/index.ts
+++ packages/discord-bot/src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 import { Client } from 'discord.js'
 
+import getDb from './db'
 import initEvents from './events'
 import logger from './logger'
 
@@ -9,6 +10,8 @@ const client = new Client()
 
 // Start the bot
 ;(async () => {
+  await getDb()
+
   client.login(process.env.DISCORD_BOT_TOKEN)
 
   client.on('ready', () => {
