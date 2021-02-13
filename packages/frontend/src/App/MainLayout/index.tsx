98ff44ffd7d12c2e61562a55d90407b9d3659c59
mawburn <mawburn7@gmail.com>
2021-02-13 00:35:35 -0500

    fix width

--- packages/frontend/src/App/MainLayout/index.tsx
+++ packages/frontend/src/App/MainLayout/index.tsx
@@ -3,15 +3,19 @@ import React, { FC } from 'react'
 import { useSelector } from 'react-redux'
 import { animated, useSpring } from 'react-spring'
 
+import useGetWidth from '../../common/hooks/useGetWidth'
 import mistWalker from '../../common/utils/mistWalker'
 import { RootState } from '../../reducers'
 import styles from './styles.module.scss'
 
 const MainLayout: FC = ({ children }) => {
   const sideBar = useSelector((state: RootState) => state.sideBar)
+  const width = useGetWidth()
+
+  const sideBarWidth = sideBar ? 350 : 68
 
   const props = useSpring({
-    gridTemplateColumns: `${sideBar ? '350px' : '68px'} auto`,
+    gridTemplateColumns: `${sideBarWidth}px ${width - sideBarWidth}px`,
   })
 
   return (
