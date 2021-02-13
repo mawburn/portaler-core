0061bf0695a53e43913ba8048ee7b43c2f9e280b
mawburn <mawburn7@gmail.com>
2021-02-13 02:35:15 -0500

    fix bot

--- packages/api-server/src/middleware/validator.ts
+++ packages/api-server/src/middleware/validator.ts
@@ -2,17 +2,22 @@ import { Request, Response, NextFunction } from 'express'
 import logger from '../utils/logger'
 
 const validator = (req: Request, res: Response, next: NextFunction) => {
-  const headers = Object.keys(req.headers).map((k) => k.toLowerCase())
+  try {
+    const headers = Object.keys(req.headers).map((k) => k.toLowerCase())
 
-  if (!headers.includes('user-agent') || !headers.includes('referer')) {
-    logger.warn('No Data', {
-      headers: JSON.parse(JSON.stringify(req.headers)),
-    })
+    if (!headers.includes('user-agent') || !headers.includes('referer')) {
+      logger.debug('No Data', {
+        headers: JSON.parse(JSON.stringify(req.headers)),
+      })
 
+      res.status(200).send({})
+    }
+
+    next()
+  } catch (err) {
+    logger.debug('Validator', { error: err })
     res.status(200).send({})
   }
-
-  next()
 }
 
 export default validator
