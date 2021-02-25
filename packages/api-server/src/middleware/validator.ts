14406a1d0d08c0838eb3588dfb9b5fef55d4e57d
mawburn <mawburn7@gmail.com>
2021-02-25 18:21:22 -0500

    remove logger

--- packages/api-server/src/middleware/validator.ts
+++ packages/api-server/src/middleware/validator.ts
@@ -1,9 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
-import logger from '../utils/logger'
 
 const validator = (req: Request, res: Response, next: NextFunction) => {
-  logger.info('User Agent', { ua: req.headers['user-agent'] })
-
   next()
 }
 
