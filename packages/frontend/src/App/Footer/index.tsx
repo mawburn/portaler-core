2938d485924b2912af4231896fa2db66862dbc72
mawburn <mawburn7@gmail.com>
2021-11-02 21:11:58 -0400

    that's all folks

--- packages/frontend/src/App/Footer/index.tsx
+++ packages/frontend/src/App/Footer/index.tsx
@@ -1,46 +1,16 @@
 import React from 'react'
 
-import {
-  discordLogo,
-  githubLogo,
-  patreonLogo,
-  twitterLogo,
-} from '../../common/images'
+import { discordLogo, githubLogo } from '../../common/images'
 import styles from './styles.module.scss'
 
 // import Twitch from './Twitch'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div className={styles.left}>
-      <a href="https://agentsofsyn.com" className={styles.aos}>
-        <img
-          src="https://agentsofsyn.com/img/logo-roll.gif"
-          alt="Agents of Syn Logo"
-        />{' '}
-        Play TTRPGs? Check out AgentsOfSyn.com
-      </a>
-    </div>
-    <div className={styles.middle}>
-      <a
-        href="https://www.patreon.com/bePatron?u=30652180"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="Gain the @Patreon Supporter role in Discord PLUS if you're a streamer get your stream posted in #streams when you go live!"
-      >
-        <img src={patreonLogo} className={styles.patreonLogo} alt="patreon" />
-      </a>
-    </div>
+    <div className={styles.left}></div>
+    <div className={styles.middle}></div>
     <div className={styles.right}>
-      <div className={styles.twitter}>
-        <a
-          href="https://twitter.com/PortalerZone"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={twitterLogo} className={styles.twitterLogo} alt="twitter" />
-        </a>
-      </div>
+      <div className={styles.twitter}></div>
       <div className={styles.github}>
         <a
           href="https://github.com/Portaler-Zone/portaler-core"
--- packages/frontend/src/Patreon/index.tsx
+++ packages/frontend/src/Patreon/index.tsx
@@ -1,79 +1,56 @@
-import { DateTime } from 'luxon'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import { Button, Dialog, DialogActions } from '@material-ui/core'
-
-import { githubLogo, patreonLogo } from '../common/images'
 import styles from './styles.module.scss'
-
-const isOctober = DateTime.now().month === 10
+import { githubLogo, thatsall } from '../common/images'
 
 const Patreon = () => {
-  const [open, setOpen] = useState<boolean>(false)
-
-  useEffect(() => {
-    if (!document.cookie.includes('patreonpop')) {
-      const displayTimer = DateTime.utc().plus({ days: isOctober ? 1 : 3 })
-
-      document.cookie = `patreonpop=true; expires=${displayTimer.toHTTP()}; SameSite=None; Secure`
-
-      if (window.location.host.split('.')[0] !== 'dwai') {
-        setOpen(true)
-      }
-    }
-  }, [])
+  const [open, setOpen] = useState<boolean>(true)
 
   return open ? (
     <Dialog open={open} aria-labelledby="alert-dialog-title">
       <div className={styles.div}>
-        <h3>Support the site!</h3>
-        <a
-          href="https://www.patreon.com/bePatron?u=30652180"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Gain the @Patreon Supporter role in Discord PLUS if you're a streamer get your stream posted in #streams when you go live!"
-        >
-          <img src={patreonLogo} className={styles.patreonLogo} alt="patreon" />
-        </a>
-        <div>This helps pay for server &amp; other related costs.</div>
-      </div>
-      <div className={styles.div}>
-        Are you a developer or know one who's looking to contribute to an open
-        source project?
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.reddit.com/r/albiononline/comments/p0qx4d/portaler_is_looking_for_code_contributors/"
-        >
-          <img src={githubLogo} className={styles.githubLogo} alt="github" />{' '}
-          Portaler is looking for contributors!
-        </a>
-        Portaler is proudly 90% TypeScript with ReactJS &amp; NodeJS.
-        {isOctober && (
-          <>
-            <h3>
-              Plus we are participating in{' '}
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://hacktoberfest.digitalocean.com/"
-              >
-                Hacktoberfest!
-              </a>
-            </h3>
+        <div>
+          <img
+            src={thatsall}
+            style={{ width: '242px', height: '242px' }}
+            alt="That's all folks"
+          />
+        </div>
+        <div>
+          <p>
+            It's been a fun ride. But, all good things have to come to an end.
+            I'll be closing up shop December 1, 2021.
+          </p>
+          <p>
+            The project will remain open source &amp; anyone is free to host
+            themselves. I will still take PRs &amp; help those who need it. But
+            I will also be shutting down the Discord server{' '}
+            <span style={{ whiteSpace: 'nowrap' }}>2021-12-01</span>.
+          </p>
+          <div>
             <a
+              href="https://github.com/Portaler-Zone/portaler-core"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://hacktoberfest.digitalocean.com/"
             >
               <img
-                src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-hacktoberfest-full.f42e3b1.svg"
-                className={styles.hacktober}
-                alt="Hacktoberfest"
+                src={githubLogo}
+                className={styles.githubLogo}
+                alt="github"
               />
+              Github Project
             </a>
-          </>
-        )}
+          </div>
+          <p>
+            All the best. This has been a great community. I can't believe it.
+            <span role="img" aria-label="heart" style={{ padding: '0 1ch' }}>
+              ❤️
+            </span>
+            <br />
+            <em>~hypnoCode</em>
+          </p>
+        </div>
       </div>
       <DialogActions>
         <Button onClick={() => setOpen(false)} color="secondary">
